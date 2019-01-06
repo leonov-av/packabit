fe4e12d4aac7e97741d90796e8f604d357d0e3a3
Alexander Leonov <me@avleonov.com>
2019-01-07 01:43:42 +0300

    creating empty dirs

--- build_nmap.sh
+++ build_nmap.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-rm -rf vagrants/nmap_ubuntu_bionic64
-mkdir vagrants/nmap_ubuntu_bionic64
-mkdir vagrants/nmap_ubuntu_bionic64/package
-cp building_scripts/nmap.sh vagrants/nmap_ubuntu_bionic64/script.sh
-cd vagrants/nmap_ubuntu_bionic64/
+distribution="ubuntu"
+release="bionic64"
+nmap_folder_name="nmap_"+$distribution+"_"+$release
+mkdir vagrants
+rm -rf vagrants/$nmap_folder_name
+mkdir vagrants/$nmap_folder_name
+mkdir vagrants/$nmap_folder_name/package
+cp building_scripts/nmap.sh vagrants/$nmap_folder_name/script.sh
+cd vagrants/$nmap_folder_name/
 vagrant init ubuntu/bionic64
 vagrant up
 vagrant ssh -c "sudo chmod +x /vagrant/script.sh; ls -al /vagrant/script.sh;"
 vagrant ssh -c "sudo /vagrant/script.sh"
 vagrant destroy -f
 cd ../../
-cp vagrants/nmap_ubuntu_bionic64/package/* packages
+mkdir packages
+cp vagrants/$nmap_folder_name/package/* packages
